f5ac9690f31888e7f67b18e1dc1c51d95bf96674
sumina-codewell <159880876+sumina-codewell@users.noreply.github.com>
2024-02-29 09:39:29 +0900

    UpDown game fin

--- UpDown.py
+++ UpDown.py
@@ -1,20 +1,41 @@
 import random
+def restart_game():
+    while True:
+        restart = input('게임을 재시작하시겠습니까? (y/n)')
+        if restart == 'n' or restart =='ㅜ':
+            print('게임을 종료합니다.')
+            exit()
+        elif restart == 'y' or restart =='ㅛ':
+            return True
+        else:
+            print('유효하지 않은 입력입니다. y 또는 n 중에 입력하세요.')
 
-com_num = random.randrange(1,101)
-print(com_num)
-
-game = int(input('1~100 사이의 숫자를 입력하세요: '))
-game != com_num
 while True:
-    if game < 1 or game > 100:
-        print('입력할 수 있는 숫자는 1부터 100까지 사이의 숫자입니다.'),
-        print(int(input('다시 입력하세요: ')))
-    elif game > com_num:
-        print('DOWN!'),
-        print(int(input('이번엔 맞힐 수 있을까요?: ')))
-    elif game < com_num:
-        print('UP!'),
-        print(int(input('이번엔 맞힐 수 있을까요?: ')))
-    else:
-        print('맞혔습니다!')
-        break
+    com_num = random.randrange(1, 101)
+    count = 1
+
+    print('1~100 사이의 숫자 하나를 입력하세요: ')
+
+    while True:
+        game = input('')
+
+        try: player = int(game)
+        except ValueError:
+            print('유효하지 않은 입력입니다. 숫자를 입력하세요.')
+            continue
+
+        if player < 1 or player > 100:
+            print('1부터 100까지 중의 숫자만 입력 가능합니다: ')
+        elif player < com_num:
+            print('UP!')
+            print('이번엔 맞힐 수 있을까요?: ')
+            count += 1
+        elif player > com_num:
+            print('DOWN!')
+            print('이번엔 맞힐 수 있을까요?: ')
+            count += 1
+        elif player == com_num:
+            print('정답입니다! '+str(count)+'회만에 성공했어요!')
+            if not restart_game():
+                exit()
+            break
